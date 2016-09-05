6910f02f9834c3f0ba7993adae0dfc42a1fe8e52
Pirlo220 <sgalvezsoler@gmail.com>
2016-09-05 23:35:39 +0200

    Some practices with XSS

--- js/matrixes.js
+++ js/matrixes.js
@@ -1,9 +1,18 @@
+var matriz = {id : 1, nombre : "Ejemplo <script>alert('EXPLOIT!!')</script>"};
 $( document ).ready(function() {
   $("div[id='searchDiv']").hide();
   $("div[id='mostrarMatrizDiv']").hide();
   $("div[id='mostrarNuevaMatrizDiv']").hide();
   $("div[id='mostrarHistoricoDiv']").hide();
   $("div[id='mostrarModificarCeldaDiv']").hide();
+  
+    /*AVOID XSS*/
+  //$("#titulo").text("Matriz: " + matriz.nombre);
+  $("#titulo").html( $.parseHTML("Nombre " +matriz.nombre));
+    
+    /*encodeURL*/
+    var url = 'http://portal.uned.es/portal/page?_pageid=93,1&_dad=portal&_schema=PORTAL';
+    $("a[id='mLink']").attr("href", encodeURIComponent(url));
 });
 
 $("#searchBtn").click(function(){
