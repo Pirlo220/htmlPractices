39549bc7530083c77941518f14fbccae4ef135ca
Pirlo220 <sgalvezsoler@gmail.com>
2016-09-05 19:19:10 +0200

    Set up some html and js with JQuery

--- js/matrixes.js
+++ js/matrixes.js
@@ -1,20 +1,40 @@
-function mostrarBusquedaMatrices() {
-	// body...
+$( document ).ready(function() {
+  $("div[id='searchDiv']").hide();
+  $("div[id='mostrarMatrizDiv']").hide();
+  $("div[id='mostrarNuevaMatrizDiv']").hide();
+  $("div[id='mostrarHistoricoDiv']").hide();
+  $("div[id='mostrarModificarCeldaDiv']").hide();
+});
+
+$("#searchBtn").click(function(){
+	 hideAllDivs();
+	$("div[id='searchDiv']").show();
+});
+
+$("#mostrarMatrixBtn").click(function(){
+	hideAllDivs();	
+	$("div[id='mostrarMatrizDiv']").show();
+});
+
+$("#nuevaMatrizBTn").click(function(){	
+	hideAllDivs();
+	$("div[id='mostrarNuevaMatrizDiv']").show();
+});
+
+$("#mostrarHistoricoBtn").click(function(){	
+	hideAllDivs();
+	$("div[id='mostrarHistoricoDiv']").show();
+});
+
+$("#modificarCeldaBtn").click(function(){	
+	hideAllDivs();
+	$("div[id='mostrarModificarCeldaDiv']").show();
+});
+
+function hideAllDivs() {
+  $("div[id='searchDiv']").hide();
+  $("div[id='mostrarMatrizDiv']").hide();
+  $("div[id='mostrarNuevaMatrizDiv']").hide();
+  $("div[id='mostrarHistoricoDiv']").hide();
+  $("div[id='mostrarModificarCeldaDiv']").hide();
 }
-
-function mostrarMatriz(matriz) {
-	// body...
-}
-
-function mostrarNuevaMatriz(){
-
-}
-
-function mostrarHistorico(dia) {
-	// body...
-}
-
-function mostrarModificarCelda() {
-	// body...
-}
-
